16413d714dca3fde2d0a06a1c278d9c5efa64618
Amir Tairbekov <amirtairbekov@gmail.com>
2024-05-15 19:04:39 +0600

    dz

--- js/home_works.js
+++ js/home_works.js
@@ -1,1 +1,86 @@
 
+
+const parentBlock = document.querySelector('.parent_block')
+const childBlock = document.querySelector('.child_block')
+
+let positionX = 0
+let positionY = 0
+
+const maxParentWidth = parentBlock.offsetWidth - childBlock.offsetWidth
+const maxParentHeight = parentBlock.offsetHeight - childBlock.offsetHeight
+
+let direction = 'right'
+
+const moveBlock = () => {
+    if (direction === 'right') {
+        if (positionX < maxParentWidth) {
+            positionX++
+        } else {
+            direction = 'down'
+        }
+    } else if (direction === 'down') {
+        if (positionY < maxParentHeight) {
+            positionY++
+        } else {
+            direction = 'left'
+        }
+    } else if (direction === 'left') {
+        if (positionX > 0) {
+            positionX--
+        } else {
+            direction = 'up'
+        }
+    } else if (direction === 'up') {
+        if (positionY > 0) {
+            positionY--
+        } else {
+            direction = 'right'
+        }
+    }
+
+    childBlock.style.left = `${positionX}px`
+    childBlock.style.top = `${positionY}px`
+    requestAnimationFrame(moveBlock)
+}
+
+moveBlock()
+
+let secondsElement = document.getElementById('seconds');
+let startButton = document.getElementById('start');
+let stopButton = document.getElementById('stop');
+let resetButton = document.getElementById('reset');
+let timerInterval;
+let seconds = 0;
+
+function startTimer() {
+    timerInterval = setInterval(updateSeconds, 1000);
+    startButton.disabled = true;
+    stopButton.disabled = false;
+}
+
+function stopTimer() {
+    clearInterval(timerInterval);
+    startButton.disabled = false;
+    stopButton.disabled = true;
+}
+
+function resetTimer() {
+    clearInterval(timerInterval);
+    seconds = 0;
+    updateDisplay();
+    startButton.disabled = false;
+    stopButton.disabled = true;
+}
+
+function updateSeconds() {
+    seconds++;
+    updateDisplay();
+}
+
+function updateDisplay() {
+    secondsElement.textContent = seconds;
+}
+
+startButton.addEventListener('click', startTimer);
+stopButton.addEventListener('click', stopTimer);
+resetButton.addEventListener('click', resetTimer);
--- js/lesson.js
+++ js/lesson.js
@@ -7,11 +7,51 @@ const regExp = /^\+996 [2579]\d{2} \d{2}-\d{2}-\d{2}$/
 
 phoneButton.onclick = () => {
     if (regExp.test(phoneInput.value)) {
-phoneResult.innerHTML = 'OK'
+        phoneResult.innerHTML = 'OK'
         phoneResult.style.color = 'green'
     } else {
         phoneResult.innerHTML = 'NOT OK'
         phoneResult.style.color = 'red'
     }
 }
+
+const tabContentBLock = document.querySelectorAll('.tab_content_block')
+const tabContentItems = document.querySelectorAll('.tab_content_item')
+const tabParent = document.querySelector('.tab_content_item')
+
+const hideTabContent = () => {
+    tabContentBLock.forEach((item) => {
+        item.style.display = 'none'
+    })
+
+    tabContentItems.forEach((item) => {
+        item.classList.remove('tab_content_item_active')
+
+    })
+
+}
+
+const showTabContent = (index = 0) => {
+    tabContentBLock [index].style.display = 'block'
+    tabContentItems[index].classList.add('tab_content_item_active')
+}
+
+hideTabContent()
+showTabContent()
+
+tabParent.onclick = (event) => {
+    if (event.target.classList.contains('tab_content_item')) {
+        tabContentItems.forEach((item, index) => {
+            if (event.target === item) {
+                hideTabContent()
+                showTabContent(index)
+            }
+        })
+    }
+}
+
+
+
+
+
 
